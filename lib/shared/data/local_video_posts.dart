22c4ab1e9bad3fc3c195d20b0ca490528058609f
Fabricio-Guima <fsgkof@gmail.com>
2024-09-22 17:03:47 -0300

    list of information's videos

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,50 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Subindo escadas rolantes',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Planta apreciada por pedestres',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Como vejo tudo embaçado!',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': 'Isso é trigo? Que interessante',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'O COVID não me afeta',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'Não quero ir trabalhar hoje, Sr. Stark',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Limpar nunca foi tão divertido',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': 'Já chegamos à Índia?... hmmm sim',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+];
